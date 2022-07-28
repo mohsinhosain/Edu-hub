4042eebeed1ae55daf516ec07ace19f1c104b4bd
mohsinhosain <mohsinhosain00@gmail.com>
2022-07-29 03:06:50 +0600

    Header completed

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {
+            spacing: {
+                '128': '43.75rem',
+            }
+        },
+
+    },
+    plugins: [],
+}
